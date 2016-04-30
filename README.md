fae152bfad4680bce4dd5563aa993ac094056a89
Emma <emma@Emmas-MacBook-Pro.local>
2016-04-30 11:07:09 -0400

    first release

---
+++ README.md
@@ -0,0 +1,31 @@
+# Exact online api demo
+## Instalation steps
+
+```bash
+
+git clone https://github.com/alexBaizeau/exact-online-api-demo.git
+cd exact-online-api-demo
+virtualenv env
+pip install -r requirements.txt
+cp config.ini.sample config.ini
+
+```
+
+
+## First time user
+Make sure that you have an app here https://apps.exactonline.com/be/fr-BE/Manage if not create a test one
+That's where the client id , secret and url are
+
+```bash
+python test_api.py setup --base-url=https://www.mycompany.com --client-id={XXXXXX-xxxx-xxxx-xxxx-XXXXXXXX} --client-secret=XXXXX
+```
+
+This step only need to be run once!!
+Open `config.ini` to see the results
+
+
+## Demo
+
+```bash
+python test_api.py demo
+```
---
+++ test_api.py
@@ -0,0 +1,54 @@
+import click
+from time import sleep
+import webbrowser
+from exactonline.api import ExactApi
+from exactonline.storage import IniStorage
+from urllib import unquote
+
+class MyIniStorage(IniStorage):
+    def get_response_url(self):
+        "Configure your custom response URL."
+        return self.get_base_url()
+
+@click.group()
+def cli():
+    pass
+
+@click.command('setup')
+@click.option('--base-url', prompt='Base url', help="Base url")
+@click.option('--client-id', prompt='Client Id', help="Client id, found in the app center")
+@click.option('--client-secret', prompt='Client secret', help="Client secret, found in the app center")
+def setup(base_url, client_id, client_secret):
+    """ One time setup of the ini storage. This onlu need to run once"""
+    storage = get_storage();
+    storage.set('application', 'base_url', base_url)
+    storage.set('application', 'client_id', client_id)
+    storage.set('application', 'client_secret', client_secret)
+    api = ExactApi(storage)
+    # Open a web browser to get the code query parameter
+    print("A web browser is going to open in 5 seconds, authenticate and copy and paste the value of the code query parameter")
+    sleep(5)
+    webbrowser.open_new(api.create_auth_request_url())
+    code = unquote(raw_input('Enter value of the code query parameter: '))
+    api.request_token(code)
+    print("High five!! it worked")
+
+@click.command('demo')
+def demo():
+    """Demo that connect to your API. and list the result of the division endpoint"""
+    api = ExactApi(get_storage())
+    api.refresh_token()
+    divisions, current_divisions = api.get_divisions()
+    print('Possible divisions {}'.format(divisions))
+    print('Current division {}'.format(current_divisions))
+
+
+def get_storage():
+    return MyIniStorage('config.ini')
+
+
+cli.add_command(demo)
+cli.add_command(setup)
+
+if __name__ == '__main__':
+    cli()
